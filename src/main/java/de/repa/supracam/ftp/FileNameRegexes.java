4203a055a70503963cc732707b37263ee0391ee9
repane <renepa82@gmail.com>
2017-02-15 22:17:59 +0100

    FileNameHandling: Group by day

---
+++ src/main/java/de/repa/supracam/ftp/FileNameRegexes.java
@@ -0,0 +1,18 @@
+package de.repa.supracam.ftp;
+
+public final class FileNameRegexes {
+    public static final String DATE_REGEX =
+            "^[1-2][0-9]{3}"
+                    + "_(0?[1-9]|[1][0-2])"
+                    + "_([1-2][0-9]|3[0-1]|0?[1-9])";
+
+    public static final String TIME_REGEX =
+            "(1[0-9]|2[0-3]|0?[0-9])"
+                    + "_(0?[0-9]|[1-5][0-9])"
+                    + "_(0?[0-9]|[1-5][0-9])";
+
+    public static final String FILE_REGEX = DATE_REGEX + "_" + TIME_REGEX + "\\.jpg";
+
+    private FileNameRegexes() {
+    }
+}
--- src/main/java/de/repa/supracam/ftp/FileNameService.java
+++ src/main/java/de/repa/supracam/ftp/FileNameService.java
@@ -6,24 +6,11 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class FileNameService {
-    private static final String DATE_REGEX =
-            "^[1-2][0-9]{3}"
-                    + "_(0?[1-9]|[1][0-2])"
-                    + "_([1-2][0-9]|3[0-1]|0?[1-9])";
-
-    private static final String TIME_REGEX =
-            "(1[0-9]|2[0-3]|0?[0-9])"
-                    + "_(0?[0-9]|[1-5][0-9])"
-                    + "_(0?[0-9]|[1-5][0-9])";
-
-    private static final String FILE_REGEX = DATE_REGEX + "_" + TIME_REGEX + "\\.jpg";
-
 
     public List<String> filerValidFileNames(List<String> fileNamesToValidate) {
         if (fileNamesToValidate != null && !fileNamesToValidate.isEmpty()) {
             return fileNamesToValidate.stream()
-                    .map(filename -> filename.substring(filename.lastIndexOf('/') + 1))
-                    .filter(fileName -> fileName.matches(FILE_REGEX))
+                    .filter(fileName -> fileName.matches(FileNameRegexes.FILE_REGEX))
                     .collect(Collectors.toList());
         }
         return Collections.EMPTY_LIST;
@@ -47,7 +34,7 @@ public class FileNameService {
     }
 
     private String getDateOfFileName(String fileName) {
-        Pattern datePatter = Pattern.compile(DATE_REGEX);
+        Pattern datePatter = Pattern.compile(FileNameRegexes.DATE_REGEX);
         Matcher matcher = datePatter.matcher(fileName);
         if (matcher.find()) {
             return matcher.group();
